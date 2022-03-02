4e8fec9e76b590cf07cd2cdea041e3619e90607d
adamstory <adamjstory@gmail.com>
2022-03-02 13:53:16 +0000

    Add phone and background to page one

--- components/Phone/Phone.js
+++ components/Phone/Phone.js
@@ -1,15 +1,8 @@
-import PhoneImage from "../../pages/images/phone_outline.svg";
-import WhiteBackground from "../../pages/images/phone_background_white.svg";
-import TaskScreenStaticOrange from "../../pages/images/task_screen_orange.svg"
-import TaskScreenStaticAqua from "../../pages/images/task_screen_aqua.svg"
+import PhoneImage from "../../pages/images/phone_task_screen.svg";
 import css from "./phone.module.css";
 
 export default function Phone({phoneDisplay}) {
 return (
-    <div className={css.container}>
-    <img className={css.phoneOuter} src={PhoneImage.src} alt=""></img>
-    <img className={css.phoneInner} src={phoneDisplay ? TaskScreenStaticOrange.src : TaskScreenStaticAqua.src} alt=""></img>
-    <img className={css.phoneBackground} src={WhiteBackground.src} alt=""></img>
-    </div>
+    <img className={css.phone} src={PhoneImage.src} alt=""></img>
 )
 }
