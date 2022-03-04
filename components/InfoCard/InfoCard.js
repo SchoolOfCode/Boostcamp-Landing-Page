aadbe27206c59aad0b2b43dc5ff53f28212433f0
adamstory <adamjstory@gmail.com>
2022-03-04 14:13:53 +0000

    Style pages 1-3 for mobile

--- components/InfoCard/InfoCard.js
+++ components/InfoCard/InfoCard.js
@@ -4,14 +4,19 @@ import BasketballSVG from "../../pages/images/basketball.svg";
 
 export default function InfoCard({ isYoga, heading, subHeading, paragraph }) {
   return (
-    <div className={isYoga ? css.yogaCard : css.basketballCard}>
-      <div className={css.svgImage}>
-        <img src={isYoga ? YogaSVG.src : BasketballSVG.src}></img>
+    <div className={css.container}>
+      <div className={isYoga ? css.yogaCard : css.basketballCard}>
+        <div className={css.svgImage}>
+          <img src={isYoga ? YogaSVG.src : BasketballSVG.src}></img>
+        </div>
+        <div className={css.textContainer}>
+          <p className={css.textHeading}>{heading}</p>
+          <p className={css.textSubHeading}>{subHeading}</p>
+          <p className={css.textParagraph}>{paragraph}</p>
+        </div>
       </div>
-      <div className={css.textContainer}>
-        <p className={css.textHeading}>{heading}</p>
-        <p className={css.textSubHeading}>{subHeading}</p>
-        <p className={css.textParagraph}>{paragraph}</p>
+      <div className={css.svgImagePhone}>
+        <img src={isYoga ? YogaSVG.src : BasketballSVG.src}></img>
       </div>
     </div>
   );
